0a2256cda9d49dac5ab438ea97ea1d5cab5dcda3
Alican Kurutepe <akurutepe@gmail.com>
2016-05-23 22:27:00 +0300

    Project initiated
    Project initiated with two source file mainly generated by netbeans
    +mainForm.java
    +VhdlEditor.java

---
+++ src/vhdleditor/VhdlEditor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vhdleditor;
+
+/**
+ *
+ * @author Alican
+ */
+public class VhdlEditor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        mainForm editorMainForm = new mainForm();
+        
+        editorMainForm.setVisible(true);
+    }
+    
+}
